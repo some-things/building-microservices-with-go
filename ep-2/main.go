825a00a676a72ed3b8e4a3a4f52d112c3652bfd2
Dustin Nemes <39643802+some-things@users.noreply.github.com>
2022-05-29 22:45:21 -0500

    wip: start ep-2

---
+++ ep-2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/some-things/building-microservices-with-go/ep-2/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	http.ListenAndServe(":9090", sm)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/some-things/building-microservices-with-go
+
+go 1.17
