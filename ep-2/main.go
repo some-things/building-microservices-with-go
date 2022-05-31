7626ef7758411e4097cb6c55905cd8087e7dce1c
Dustin Nemes <39643802+some-things@users.noreply.github.com>
2022-05-30 20:19:47 -0500

    complete ep-2

--- ep-2/main.go
+++ ep-2/main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/some-things/building-microservices-with-go/ep-2/handlers"
 )
 
 func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	l := log.New(os.Stdout, "product-api: ", log.LstdFlags)
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
+
+	// Register handlers to serve mux
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// Wrap this in go func so it is non blocking
+	// Start the server
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// Trap sigterm or interupt and gracefully shutdown the server
+	sc := make(chan os.Signal)
+	signal.Notify(sc, os.Interrupt)
+	signal.Notify(sc, os.Kill)
+
+	// Block until signal is received
+	sig := <-sc
+	l.Printf("Received %s signal, shutting down.", sig)
+
+	// Gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(ctx)
 }
